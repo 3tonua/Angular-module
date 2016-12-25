7803d89d82b3312033e637c044b09e99ca80631b
Anton <3ton.ua@gmail.com>
2016-12-25 20:33:10 +0200

    Delete routes.js

--- js/routes.js
+++
@@ -1,9 +0,0 @@
-app.config(function ($routeProvider) {
-    $routeProvider.when('/', {
-        controller: 'allController',
-        templateUrl: 'templates/all-users.html'
-    }).when('/open-user/:username', {
-        controller: 'userController',
-        templateUrl: 'templates/open-user.html'
-    })
-});
